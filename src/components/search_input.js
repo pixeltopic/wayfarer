b98122b75052bc65796b3a388e8869f8eb0670e0
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2018-04-02 01:17:43 -0700

    Form Updates
    Created Select and added to form.

--- src/components/search_input.js
+++ src/components/search_input.js
@@ -1,43 +1,85 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 // import { fetchDirections } from "../actions/actionTypes";
+import { Well } from "react-bootstrap";
+
+
+// TODO: Form Styling, Validation, Parameter Inputs
+
 
 class SearchInput extends Component {
     renderInput(field) {
+        // console.log(field);
         // renders origin and destination text input fields
         
         return (
             <div>
-            <label>{field.label}</label>
-                <input type="text" className="form-control" {...field.input}/>
+                <input type="text" className="form-control" placeholder={field.placeholder} {...field.input}/>
             </div>
         );
     }
 
-    renderDropDown() {
+    createSelectArray(...choices) {
+        let key = 0;
+        return choices.map(
+            choice => {
+                return <option key={key++} value={choice.toLowerCase()}>{choice}</option>
+            }
+        );
+    }
+
+    renderSelect(field) {
         // renders drop down menu
+        // console.log("Select Field", field);
+        return (
+            <div>
+                <label>{field.label}</label>
+                <select className="form-control custom-select" {...field.input}>
+                    {field.selectArray}
+                </select>
+            </div>
+        );
     }
 
     // render checklist/check for form
+
+    renderChecklist() {
+
+    }
 
     onSubmit(values) {
         console.log(values);
     }
 
     render() {
+        const travel_array = this.createSelectArray("Driving", "Bicycling", "Walking", "Transit");
         const { handleSubmit } = this.props;
         return (
-            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+            <Well>
+            <form className="form-group" onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 
                 <Field
                 name="origin"
-                label="Origin"
+                placeholder="Enter Origin"
                 component={this.renderInput}
+                />
+
+                <Field
+                name="destination"
+                placeholder="Enter Destination"
+                component={this.renderInput}
+                />
+
+                <Field
+                name="travelmode"
+                label="Enter a mode of travel:"
+                selectArray={travel_array}
+                component={this.renderSelect}
                 />
 
                 <button type="submit" className="btn btn-primary">Search</button>
             </form>
-
+            </Well>
         );
 
     }
