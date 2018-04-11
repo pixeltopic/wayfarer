e89609e117dd994d54fbc3bd24801eea860953a3
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2018-04-11 14:51:29 -0700

    Redux-Form and React-Bootstrap
    Successfully combined Redux-Form and React-Bootstrap.

--- src/components/search_input.js
+++ src/components/search_input.js
@@ -2,44 +2,123 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 import { fetchDirections, updateForm } from "../actions/actionTypes";
 import { connect } from "react-redux";
-import { Well } from "react-bootstrap";
+// import { Well } from "react-bootstrap";
+import { Form, FormGroup, ControlLabel, FormControl, Button } from "react-bootstrap";
 
 
-// TODO: Form Styling, Validation, Parameter Inputs ( checlist needs fixing )
-// FIX initial values - use initialValues props and enableReinitialize
+// TODO: Form Styling, Validation, Parameter Inputs
 
+// why won't inline styling on form work?
+// finish URL builder action creator for fetching directions
+// FIX initial values - use initialValues props and enableReinitialize (DONE!)
+
+// { input, meta, custom prop parameters, ...props } must always be a parameter.
+const renderInput = ({input, meta, name, type, placeholder, label, ...props}) => {
+    return (
+        <div>
+            <ControlLabel>{label}</ControlLabel>{' '}
+            <FormControl 
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                {...props} {...input}
+            />
+        </div>
+    )
+}
+
+const renderSelect = ({ input, meta, name, selectArray, label, ...props }) => {
+    return (
+        <div>
+            <ControlLabel>{label}</ControlLabel>{' '}
+            <FormControl 
+                componentClass="select"
+                name={name}
+                {...props} {...input}>
+                {selectArray}
+            </FormControl>
+        </div>
+    )
+}
 
 class SearchInput extends Component {
-    // componentWillMount() {
-    //     const data = {
-    //         travelmode: "driving",
-    //         alternativeroute: "false", 
-    //         unit: "imperial", 
-    //     };
-    //     this.props.updateForm(data);
+    // constructor(props) {
+    //     super(props);
+    //     //this.handleChange = this.handleChange.bind(this);
+    //     // this.handleSubmit = this.handleSubmit.bind(this);
     // }
 
+    onSubmit(values) {
+        // uses callback from App to force-update parent (App's) state.
+        console.log("Form submitted"); 
+        console.log(values);
+    }
+
+    render() {
+        const { handleSubmit } = this.props;
+        return(
+            <Form onSubmit={handleSubmit(this.onSubmit.bind(this))} inline>
+                <FormGroup controlId="formInlineOrigin">
+                    <Field name="originInput" type="text" placeholder="e.g. Irvine" label="Origin Input" component={renderInput} />
+                </FormGroup>{' '}
+
+                <FormGroup controlId="formInlineDestination">
+                    <Field name="destinationInput" type="text" placeholder="e.g. Anaheim" label="Destination Input" component={renderInput} />
+                </FormGroup>{' '}
+
+                <Button type="submit">Find Directions</Button>
+            </Form>
+        );
+
+    }
+}
+
+function validate(values) {
+    const errors = {};
+    return errors;
+}
+
+function mapStateToProps(state) {
+    return {
+        initialValues: {originInput: "Irvine"},
+        formContent: state.formContent
+    };
+}
+
+export default connect(mapStateToProps, { fetchDirections, updateForm })(
+    reduxForm({ 
+        validate: validate, 
+        form: "SearchInputForm",
+        enableReinitialize : true
+    })(SearchInput)
+);
+
+
+
+
+
+/*
+class SearchInput extends Component {
     renderInput(field) {
         // renders origin and destination text input fields
-        
         return (
             <div>
                 <input type="text" className="form-control" placeholder={field.placeholder} {...field.input}/>
             </div>
         );
     }
-
     createSelectArray(...choices) {
+        // returns an arr of select JSX, used as a prop for renderSelect method
         let key = 0;
         return choices.map(
             choice => {
+                const displayText = choice;
                 choice.toLowerCase() === "no" ? choice = "False" : {};
                 choice.toLowerCase() === "yes" ? choice = "True" : {};
-                return <option key={key++} value={choice.toLowerCase()}>{choice}</option>
+                return <option key={key++} value={choice.toLowerCase()}>{displayText}</option>
             }
         );
     }
-
     renderSelect(field) {
         // renders drop down menu
         // console.log("Select Field", field);
@@ -53,35 +132,18 @@ class SearchInput extends Component {
         );
     }
 
-    createCheckArray(...checkboxes) {
-        let key = 0;
-        return checkboxes.map(checkbox => {
-            return (
-                <div className="form-check form-check-inline" key={key++}>
-                    <input className="form-check-input" type="checkbox" id={`inlineCheckbox${key}`} value={checkbox.toLowerCase()} />
-                    <label className="form-check-label" htmlFor={`inlineCheckbox${key}`}>{checkbox}</label>
-                </div>
-            );
-        });
-    }
-
-    renderChecklist(field) {
+    renderCheck(field) {
         return (
-            <div {...field.input}>
-                <label>{field.label}</label>
-                {field.checkArray}
+            <div className="form-check form-check-inline" {...field.input}>
+                <input className="form-check-input" type="checkbox" id={field.id_val} value={field.label.toLowerCase()} />
+                <label className="form-check-label" htmlFor={field.id_val}>{field.label}</label>
             </div>
         );
     }
-    /*
-    const avoid_array = this.createCheckArray("Tolls", "Highways", "Ferries", "Indoor");
-<Field name="avoid" label="Select features to avoid on routes." 
-checkArray={avoid_array} component={this.renderChecklist} />
-    */
 
     onSubmit(values) {
         this.props.updateForm(values);
-        this.props.fetchDirections(values.origin, values.destination);
+        // this.props.fetchDirections(values.origin, values.destination);
         console.log(values);
         console.log(this.props.formContent);
     }
@@ -98,15 +160,21 @@ checkArray={avoid_array} component={this.renderChecklist} />
 
                 <Field name="origin" placeholder="Enter Origin" component={this.renderInput} />
                 <Field name="destination" placeholder="Enter Destination" component={this.renderInput} />
-
-                <Field name="travelmode" label="How would you like to travel by?"
+                
+                <Field name="travelmode" label="Travel Method "
                 selectArray={travel_array} component={this.renderSelect} />
 
-                <Field name="alternativeroute" label="Do you want alternative routes?"
+                <Field name="alternativeroute" label="Request Alternative Routes "
                 selectArray={alt_array} component={this.renderSelect} />
 
-                <Field name="unit" label="Display results in Imperial or Metric units?"
+                <Field name="unit" label="Unit Display "
                 selectArray={unit_array} component={this.renderSelect} />
+                
+                <label>Features to avoid on routes</label>
+                <Field name="avoid_tolls" label="Tolls" id_val="tolls" component={this.renderCheck} />
+                <Field name="avoid_highways" label="Highways" id_val="highways" component={this.renderCheck} />
+                <Field name="avoid_ferries" label="Ferries" id_val="ferries" component={this.renderCheck} />
+                <Field name="avoid_indoor" label="Indoor" id_val="indoor" component={this.renderCheck} />
 
                 <button type="submit" className="btn btn-primary">Search</button>
             </form>
@@ -116,8 +184,17 @@ checkArray={avoid_array} component={this.renderChecklist} />
     }
 }
 
-function validate() {
+function validate(values) {
     const errors = {};
+
+    if (!values.origin) {
+        errors.origin = "Enter a valid origin!"
+    }
+
+    if (!values.destination) {
+        errors.destination = "Enter a valid destination!";
+    }
+
 
     return errors;
 }
@@ -140,7 +217,7 @@ export default connect(mapStateToProps, { fetchDirections, updateForm })(
         enableReinitialize : true
     })(SearchInput)
 );
-
+*/
 
 // function mapStateToProps(state) {
 //     return {formContent: state.formContent};
