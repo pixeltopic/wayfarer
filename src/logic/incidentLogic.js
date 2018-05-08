e1db4e588b3d29dcd9e5c93ae0c240cd0aa3e525
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2018-05-07 22:14:21 -0700

    Segmenting Algorithm Completion
    Code now divides areas of < 50,000 square miles correctly.

--- src/logic/incidentLogic.js
+++ src/logic/incidentLogic.js
@@ -43,7 +43,7 @@ export const genSegmentObj = (latLngArr) => {
     let result = [];
     const kmSq = 129499;
     let tempArr = latLngArr;
-    let corner1, corner2, length, width, i, prev;
+    let corner1, corner2, length, width, i, prev = 0;
 
     // initial check: assert origin to destination is under 50,000 sq miles.
     const origin = tempArr[0];
@@ -57,15 +57,28 @@ export const genSegmentObj = (latLngArr) => {
     while (tempArr.length !== 0) {
         corner1 = tempArr.shift(); // shift shortest corner out from front index
         for (i = 0; i < tempArr.length; i++) {
-            console.log("tempArr length=",tempArr.length);
             corner2 = tempArr[i];
             length = getDistance(corner1, { lat: corner1.lat, lng: corner2.lng }) / 1000;
             width = getDistance(corner2, { lat: corner1.lat, lng: corner2.lng }) / 1000;
-            console.log("debug: l*w:", length*width, "< 129499");
+            // console.log("debug: l*w:", length*width, "< 129499");
             if (length*width > kmSq) {
                 console.log("debug: l*w exceeded:", length*width, "< 129499");
-                corner2 = tempArr[prev];
-                break;
+                // dlength and dwidth ensure that the prev value * current value is valid, or else
+                // the algorithm will end prematurely with invalid values.
+                // if dlength * dwidth > kmSq, then the algorithm will skip that bounding box entirely.
+                const dlength = getDistance(corner1, { lat: corner1.lat, lng: tempArr[prev].lng}) / 1000;
+                const dwidth = getDistance(tempArr[prev], { lat: corner1.lat, lng: tempArr[prev].lng }) / 1000;
+                if (dlength*dwidth > kmSq) {
+                    console.log("debug: dl*dw exceeded:", dlength*dwidth, "< 129499");
+                    corner1 = tempArr.shift(); // overwrite old corner1
+                    corner2 = corner1; 
+                    // previous corner2 overwritten, don't need.
+                    // The next iteration area will be 0 for dlength and dwidth
+                } else {
+                    console.log("debug2: l*w should not exceed:", dlength*dwidth, "< 129499");
+                    corner2 = tempArr[prev];
+                    break;
+                }
             }
             prev = i;
         }
